24ad3b67280a3d5f85ebd00e24603480581967fc
victoriatytar <52772164+victoriatytar@users.noreply.github.com>
2019-08-04 23:24:45 +0300

    Update TasksJS_05.08.js

--- TasksJS_05.08.js
+++ TasksJS_05.08.js
@@ -1,11 +1,18 @@
-https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript?fbclid=IwAR0Ri57rTbNaLdLxCKpoq0PkJ4BagazF7RrkGTt4rpgYYNuD-MPRlSzxBRU
+1)https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript?fbclid=IwAR0Ri57rTbNaLdLxCKpoq0PkJ4BagazF7RrkGTt4rpgYYNuD-MPRlSzxBRU
 
-  function buildFun(n){
-	  var res = []
-	  for (var i = 0; i< n; i++){
-      let i2 = i;
-		  res.push(function(){
-			  return i2;
-		  })
-	  }
-	  return res
+function buildFun(n) {
+    var res = [];
+    for (var i = 0; i< n; i++){
+        let i2 = i;
+        res.push(function(){
+            return i2;
+        })
+    }
+    return res;
+}	  
+	  
+2)https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript?fbclid=IwAR1sjjQrcXeT_PLRiCzCchw7E26icmAlp8kLMNHZdZZrT4FUY9_T4BjoxWw	  
+	
+function circleCircumference(circle) {
+    return 2 * Math.PI * circle.radius;
+}
