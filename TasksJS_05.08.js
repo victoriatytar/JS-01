597587a495c785b92abe5ffeca2c392b0d959991
victoriatytar <52772164+victoriatytar@users.noreply.github.com>
2019-08-05 10:17:15 +0300

    Update TasksJS_05.08.js

--- TasksJS_05.08.js
+++ TasksJS_05.08.js
@@ -16,3 +16,22 @@ function buildFun(n) {
 function circleCircumference(circle) {
     return 2 * Math.PI * circle.radius;
 }
+
+
+3)https://www.codewars.com/kata/training-js-number-13-number-object-and-its-properties/train/javascript?fbclid=IwAR2P8n5YF7nRjpEmRj2WUPwl8JHEq6Ho4JMoEpSq3LnENd-JJ5vqM6i2Cvk
+
+function whatNumberIsIt(n) {
+    if (n === Number.MAX_VALUE) {
+        return "Input number is Number.MAX_VALUE";
+    } else if (n === Number.MIN_VALUE) {
+        return "Input number is Number.MIN_VALUE";
+    } else if (Number.isNaN(n)) {
+        return "Input number is Number.NaN";
+    } else if (n === Number.NEGATIVE_INFINITY) {
+        return "Input number is Number.NEGATIVE_INFINITY";
+    } else if (n === Number.POSITIVE_INFINITY) {
+        return "Input number is Number.POSITIVE_INFINITY";
+    } else {
+        return "Input number is " + n;
+    }
+};
